1350be363de0b7b06eb1830e1430ef6f930acd91
Prasadcatna <58583049+Prasadcatna@users.noreply.github.com>
2022-09-05 06:35:07 +0530

    Update current.cpp

--- current.cpp
+++ current.cpp
@@ -6,5 +6,24 @@ Current :: Current(){
 }
 
 void Current :: withdraw(){
-	std::cout<<"Current withdraw";
+	// double ba = balance + od_limit;
+	double wdAmt;
+	std::cout<< "Enter Amount to be Withdraw: ";
+	std::cin>>wdAmt;
+	if(balance > wdAmt ){
+		balance -= wdAmt;
+	}
+	else if(balance == 0){
+		std::cout<<"There is no Fund in you'r Account\n";
+	}else{
+		std::cout<<"You'r existing the OverDraft limit\n Do you want to continue(y/n)";
+		char ch;
+		std::cin>>ch;
+		if(ch == 'y'){
+			balance +=od_limit;
+			balance -= wdAmt;
+		}else if(ch == 'n'){
+			std::cout<<"Thank you";
+		}
+	}
 }
