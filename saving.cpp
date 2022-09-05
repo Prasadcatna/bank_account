7c89a22f3708b78d83a73722a9fd1c3521de4f63
Prasadcatna <58583049+Prasadcatna@users.noreply.github.com>
2022-09-05 06:35:39 +0530

    Update saving.cpp

--- saving.cpp
+++ saving.cpp
@@ -10,7 +10,24 @@ void Saving :: getMinBal(){
 }
 
 void Saving :: withdraw(){
-	std::cout<<"Saving withdraw";
+	double ba = balance - minBal;
+	double wdAmt;
+	std::cout<< "Enter Amount to be Withdraw: ";
+	std::cin>>wdAmt;
+	if(ba > wdAmt ){
+		balance -= wdAmt;
+	}else if(balance == 0){
+		std::cout<<"There is no Fund in you'r Account\n";
+	}else{
+		std::cout<<"You'r existing the Minimum Balance limit\n Do you want to continue(y/n)";
+		char ch;
+		std::cin>>ch;
+		if(ch == 'y'){
+			balance -= wdAmt;
+		}else if(ch == 'n'){
+			std::cout<<"Thank you";
+		}
+	}
 }
 
 
